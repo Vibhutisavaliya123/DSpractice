e7efb0ccfa447b6394e246acba815738e8db3ca9
Vibhutisavaliya123 <60962915+Vibhutisavaliya123@users.noreply.github.com>
2020-10-26 10:54:53 +0530

    DS practicel  5.py

--- DS practicel  5.py
+++ DS practicel  5.py
@@ -22,16 +22,72 @@ else:
     print("Element is present at index", result);
 
 
-Code binary
-Def binary_search(list value_, search,start,end):
-    if start > end:
-       return -1
-    mid=(start + end)//2
-    if list_values[mid] == search:
-        return mid
-    if element < list_values [mid]:
-       return binary_search(list_values, search,start,mid+1) 
-else:
-      return binary_search(list_values, search,start,mid-1,end))
+Code binary:-
+def binary_search(arr, low, high, x): 
 
-    
+  
+
+    # Check base case 
+
+    if high >= low: 
+
+  
+
+        mid = (high + low) // 2
+
+  
+
+        # If element is present at the middle itself 
+
+        if arr[mid] == x: 
+
+            return mid 
+
+  
+
+        # If element is smaller than mid, then it can only 
+
+        # be present in left subarray 
+
+        elif arr[mid] > x: 
+
+            return binary_search(arr, low, mid - 1, x) 
+
+  
+
+        # Else the element can only be present in right subarray 
+
+        else: 
+
+            return binary_search(arr, mid + 1, high, x) 
+
+  
+
+    else: 
+
+        # Element is not present in the array 
+
+        return -1
+
+  
+# Test array 
+
+arr = [ 2, 3, 4, 10, 40 ] 
+
+x = 10
+
+  
+# Function call 
+
+result = binary_search(arr, 0, len(arr)-1, x) 
+
+  
+
+if result != -1: 
+
+    print("Element is present at index", str(result)) 
+
+else: 
+
+    print("Element is not present
+
